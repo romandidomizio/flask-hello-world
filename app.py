7105e94ac4edace83de31b61ec63be415b8a1a5f
Roman Didomizo <rodi1364@colorado.edu>
2023-08-02 02:33:53 +0000

    drop route added. lab complete

--- app.py
+++ app.py
@@ -63,3 +63,12 @@ def selecting():
     response_string += "</table>"
 
     return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgres://lab_10_database_user:UwufnvTL5ONEPaEj6UH6xZtOMTnjJeXL@dpg-cj4rd3icn0vc73fh7940-a/lab_10_database")
+    cur = conn.cursor()
+    cur.execute('DROP TABLE Basketball;')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
